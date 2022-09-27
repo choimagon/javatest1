516e4251c553619f9abb1b7ca6a8aafa0ea3fd48
choejihun <magon8696@naver.com>
2022-09-27 17:07:32 +0900

    9월27일 커밋

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+
+import java.lang.String;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int a=10;
-        int b=20;
-        System.out.println(a+b);
+    public static void main(String[] args) throws IOException{
+        System.out.println("------------------start----------------------\n");
+        String URL = "https://weather.naver.com/today/03127250?cpName=KMA";
+        Document doc = Jsoup.connect(URL).get();
+
+        Elements elem = doc.select(".current");
+        System.out.println(elem.text());
+        System.out.println("\n------------------End----------------------");
         //두번째
     }
 }
